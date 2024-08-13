f0de3282346a646db4a177c8ea98173ca807b665
llpingll <94099281+llpingll@users.noreply.github.com>
2024-08-13 13:11:20 +0100

    Add db connection | install passport and dependancies

---
+++ server/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+// Load environment variables from .env file if not in production
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+// Get the database connection string
+const DB = process.env.DATABASE_URI;
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(DB);
+    console.log("Connected to database");
+  } catch (error) {
+    console.log("Failed to connect to database");
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectToDB;
